9f48ec3c0c33702730feca1b120a6cfe15f08a6f
David Bismut <david.bismut@gmail.com>
2019-11-01 16:46:19 +0100

    Update wheel exmaple

--- demos/hooks/wheel/index.js
+++ demos/hooks/wheel/index.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import { useSpring, animated } from 'react-spring'
-import { useWheel } from 'react-use-gesture'
+import { useGesture } from 'react-use-gesture'
 import { clamp } from 'lodash'
 import './styles.css'
 
 export default function Wheel() {
-  const [{ clampY, localY }, set] = useSpring(() => ({ clampY: 0, offsetY: 0 }))
+  const [{ wheel, zoom }, set] = useSpring(() => ({
+    wheel: 0,
+    zoom: 0,
+  }))
 
-  const bind = useWheel(
-    ({ offset: [, oy], movement: [, my], memo = clampY.getValue() }) => {
-      set({ clampY: clamp(my + memo, -500, 500), offsetY: oy, immediate: true })
-      return memo
-    }
+  const domTarget = React.useRef(null)
+
+  const bind = useGesture(
+    {
+      onWheel: ({ offset: [, oy] }) => {
+        set({ wheel: oy, immediate: true })
+      },
+      onPinch: ({ offset: [z] }) => {
+        set({ zoom: z, immediate: true })
+      },
+    },
+    { domTarget, event: { passive: false } }
   )
 
+  React.useEffect(bind, [bind])
+
   return (
-    <div className="wheel" {...bind()}>
-      <animated.div>{clampY}</animated.div>
-      <animated.div>{localY}</animated.div>
+    <div ref={domTarget} className="wheel">
+      <animated.div>{wheel}</animated.div>
+      <animated.div>{zoom}</animated.div>
     </div>
   )
 }
